0a5a4a317df87e3d9b62ca1b92cb37d64c199cd9
赵睿 <zrendless@foxmail.com>
2021-04-26 17:57:53 +0800

    const()括号内,下一行省略则自动赋值为上一行的值,iota特殊常量,编译器自动递增

--- main.go
+++ main.go
@@ -5,22 +5,17 @@ import (
 )
 
 const (
-	c=8
-	d=9
+	a=iota
+	b
+	c="test"
+	d
+	e=100
+	f
+	g=iota
+	h
 )
 
-const e=88
-
 func main() {
-	//var a,b int= 1,2//可以声明int类型,也可以隐藏
-	const a,b=1,2
-
-	//覆盖外部的
-	const (
-		c=18
-		d=19
-	)
-
-	fmt.Println(a,b,c,d,e)
+	fmt.Println(a,b,c,d,e,f,g,h)
 }
 
