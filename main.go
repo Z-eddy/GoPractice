730dc52d85f0611a15c7c3100d4cce43c5bbebf0
赵睿 <zrendless@foxmail.com>
2021-04-26 16:48:44 +0800

    字符串通过+可以连接

--- main.go
+++ main.go
@@ -4,24 +4,6 @@ import (
 	"fmt"
 )
 
-func log(msg string){
-	for i := 0 ; i < 100; i++ {
-		fmt.Println(msg,i)
-	}
-}
-
-func init(){
-	fmt.Println("initialize...")
-}
-
-func init(){
-	fmt.Println("init 1")
-}
-
 func main() {
-	//自动多线程
-	go log("A:")
-	go log("B:")
-
-	fmt.Println("finish")
+	fmt.Println("test"+"other")
 }
