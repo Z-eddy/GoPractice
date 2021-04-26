26e17b6d47479d6f4205b2a62b1680dc5b899209
赵睿 <zrendless@foxmail.com>
2021-04-26 10:56:30 +0800

    fmt格式化并打印消息

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func log(msg string){
+	for i := 0 ; i < 100; i++ {
+		fmt.Println(msg,i)
+	}
+}
+
+func main() {
+	//自动多线程
+	go log("A:")
+	go log("B:")
+
+	fmt.Println("finish")
+}
