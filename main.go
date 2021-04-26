208b94de86d448ff2e1b2c79f71b769e6ad64fd5
赵睿 <zrendless@foxmail.com>
2021-04-26 17:51:46 +0800

    常量定义

--- main.go
+++ main.go
@@ -4,9 +4,23 @@ import (
 	"fmt"
 )
 
+const (
+	c=8
+	d=9
+)
+
+const e=88
+
 func main() {
 	//var a,b int= 1,2//可以声明int类型,也可以隐藏
-	var a,b = 1,2
-	fmt.Println("start",a,b)
+	const a,b=1,2
+
+	//覆盖外部的
+	const (
+		c=18
+		d=19
+	)
+
+	fmt.Println(a,b,c,d,e)
 }
 
