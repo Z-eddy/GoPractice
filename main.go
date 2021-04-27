45cd23aa2d19fdec70a3350be3fe159f590cb547
赵睿 <zrendless@foxmail.com>
2021-04-27 16:20:18 +0800

    带缓存通道,使用for访问并获得通道传递的值,必须最后close,否则会永久阻塞

--- main.go
+++ main.go
@@ -2,30 +2,22 @@ package main
 
 import "fmt"
 
-func sum(a uint32,c chan uint32){
-	var i,total uint32
-	total=0
+func send(a uint32,c chan uint32){
+	var i uint32
 	for i= 0;i!=a;i++ {
-		total+=i
+		c <- i
 	}
 
-	c <- total
+	close(c)
 }
 
 func main() {
-	var a,b,d uint32
-	a=0
-	b=10
-	d=20
+	c:=make(chan uint32,100)
 
-	c:=make(chan uint32)
+	send(20,c)
 
-	go sum(a,c)
-	go sum(b,c)
-	go sum(d,c)
-
-	x,y,z:=<-c,<-c,<-c
-
-	fmt.Println(x,y,z)
+	for v:=range c{
+		fmt.Println(v)
+	}
 }
 
