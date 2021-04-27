6871caa4f8c39c0b08bc0ebccb2cc373390e714a
赵睿 <zrendless@foxmail.com>
2021-04-27 10:53:11 +0800

    函数参数指针传递

--- main.go
+++ main.go
@@ -2,17 +2,13 @@ package main
 
 import "fmt"
 
-func foo(sA string,b int)(int,string){
-	return b+1,sA+"post"
-}
-
-func fooAdd(a,b int)int{
-	return a+b
+func fooAdd(a *int){
+	*a += 1
 }
 
 func main() {
-	a,b:=foo("myTest",9)
-	c:=fooAdd(2,3)
-	fmt.Println(a,b,c)
+	a:=3
+	fooAdd(&a)
+	fmt.Println(a)
 }
 
