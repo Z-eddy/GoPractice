16497599cd39e0eefa26c243da9c0e23a00a9241
赵睿 <zrendless@foxmail.com>
2021-04-27 09:54:53 +0800

    switch语句

--- main.go
+++ main.go
@@ -5,7 +5,20 @@ import (
 )
 
 func main() {
-	a,b:=2,3
-	fmt.Println(a>b)
+	const a,b,c=10,"test",10.8
+	switch a{
+	case 0:fmt.Println("0")
+	case 1:fmt.Println("1")
+	case 10:
+		fmt.Println("my 10")
+		fallthrough
+	case 2:fmt.Println("2")
+	case 3:fmt.Println("3")
+	}
+
+	switch{
+	case true:fmt.Println("true")
+	case false:fmt.Println("false")
+	}
 }
 
