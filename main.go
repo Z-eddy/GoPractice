f52eefe3bf05fe2382be3bd685732270c6393515
赵睿 <zrendless@foxmail.com>
2021-04-27 14:08:55 +0800

    slice动态数组,make创建,部分索引创建子数组,len查看元素数,cap查看容量,copy深拷贝,append增加元素

--- main.go
+++ main.go
@@ -2,18 +2,36 @@ package main
 
 import "fmt"
 
-type Book struct{
-	name string
-	author string
-}
-
-func foo(myBook *Book){
-	fmt.Println(myBook.name)
-	fmt.Println(myBook.author)
-}
-
 func main() {
-	theBook:=Book{name:"nameA",author:"zr"}
-	foo(&theBook)
+	//直接赋值创建
+	dirArr:=[]int{1,2,3}
+	//make创建8个int元素的数组
+	arr0:=make([]int,20)
+	//复制整个引用
+	arr1:=dirArr[:]
+	arr2:=arr0
+	//引用部分索引
+	arr3:=arr2[1:]
+	arr4:=arr3[:15]
+	arr5:=arr4[0:12]
+
+	//元素访问
+	arr1[2]=12
+	arr0[4]=88
+
+	fmt.Println(arr5)
+
+	//查看len/cap
+	//fmt.Println(len(arr0),cap(arr0))
+	//fmt.Println(len(arr2),cap(arr2))
+	//fmt.Println(len(arr3),cap(arr3))
+	//fmt.Println(len(arr4),cap(arr4))
+	//fmt.Println(len(arr5),cap(arr5))
+
+	//拷贝数组
+	arrDest:=make([]int,len(arr5),cap(arr5)*2)
+	copy(arrDest,arr5)
+	arr5[1]=11
+	fmt.Println(arrDest,arr5)
 }
 
