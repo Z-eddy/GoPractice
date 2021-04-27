e7ca831452a00f57b108e8b7af702018b4a0d4a0
赵睿 <zrendless@foxmail.com>
2021-04-27 15:40:43 +0800

    函数返回error

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+func divideErr(a,b float64)(float64,error){
+	if b!=0 {
+		return a / b, nil
+	}else{
+		return 0,errors.New("error!!!")
+	}
+}
 
 func main() {
-	//myMap:=make(map[int]string)
-	myMap:=map[int]string{}
-	myMap[0]="my0"
-	myMap[1]="my1"
-	myMap[2]="my2"
-
-	for key,val:=range myMap {
-		fmt.Println(key,val)
-	}
-
-	delete(myMap,1)
-	fmt.Println(myMap)
+	fmt.Println(divideErr(5,0))
 }
 
