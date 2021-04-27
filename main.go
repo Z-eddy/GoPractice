66a5fbd34d5e0576ee8a2d1891358fe176c84372
赵睿 <zrendless@foxmail.com>
2021-04-27 11:37:34 +0800

    结构体,指针也使用.访问成员

--- main.go
+++ main.go
@@ -2,35 +2,18 @@ package main
 
 import "fmt"
 
-func arrAdd(arr *[]int)int{
-	var total int=0
-	for _,val:=range *arr{
-		total+=val
-	}
+type Book struct{
+	name string
+	author string
+}
 
-	return total
+func foo(myBook *Book){
+	fmt.Println(myBook.name)
+	fmt.Println(myBook.author)
 }
 
 func main() {
-	arr0:=[]int{1,2,3}
-	fmt.Println(arrAdd(&arr0))
-
-	oneDArr:=[]int{1,2,3}
-	for i,val:=range oneDArr {
-		fmt.Println(i,val)
-	}
-
-	arrA:=[][]int{
-		{1,2,3},
-		{4,5,6},
-	}
-	row0:=[]int{7,8,9}
-	arrA=append(arrA,row0)
-
-	for i,val := range arrA {
-		for j,jVal := range val{
-			fmt.Println(i,j,jVal)
-		}
-	}
+	theBook:=Book{name:"nameA",author:"zr"}
+	foo(&theBook)
 }
 
