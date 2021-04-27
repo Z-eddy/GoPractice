a8c78821821008ee64c6ac9da2dd69f645a82430
赵睿 <zrendless@foxmail.com>
2021-04-27 14:54:58 +0800

    map键值对,delete删除元素

--- main.go
+++ main.go
@@ -3,35 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	//直接赋值创建
-	dirArr:=[]int{1,2,3}
-	//make创建8个int元素的数组
-	arr0:=make([]int,20)
-	//复制整个引用
-	arr1:=dirArr[:]
-	arr2:=arr0
-	//引用部分索引
-	arr3:=arr2[1:]
-	arr4:=arr3[:15]
-	arr5:=arr4[0:12]
+	//myMap:=make(map[int]string)
+	myMap:=map[int]string{}
+	myMap[0]="my0"
+	myMap[1]="my1"
+	myMap[2]="my2"
 
-	//元素访问
-	arr1[2]=12
-	arr0[4]=88
+	for key,val:=range myMap {
+		fmt.Println(key,val)
+	}
 
-	fmt.Println(arr5)
-
-	//查看len/cap
-	//fmt.Println(len(arr0),cap(arr0))
-	//fmt.Println(len(arr2),cap(arr2))
-	//fmt.Println(len(arr3),cap(arr3))
-	//fmt.Println(len(arr4),cap(arr4))
-	//fmt.Println(len(arr5),cap(arr5))
-
-	//拷贝数组
-	arrDest:=make([]int,len(arr5),cap(arr5)*2)
-	copy(arrDest,arr5)
-	arr5[1]=11
-	fmt.Println(arrDest,arr5)
+	delete(myMap,1)
+	fmt.Println(myMap)
 }
 
