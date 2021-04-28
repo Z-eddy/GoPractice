bc4c7b033986fce70efe96a397aca9193b30b539
赵睿 <zrendless@foxmail.com>
2021-04-28 10:08:30 +0800

    sync.WaitGroup信号量进行同步

--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"sync"
+)
 
 func main() {
-	func(a int){
-		fmt.Println(a+1)
-	}(5)
+	log.SetOutput(os.Stdout)
+
+	myChl:=make(chan int)
+
+	wait:=sync.WaitGroup{}
+	wait.Add(1)//计数器增加goroutine数量
+
+	go func()int{
+		var sum int = 0
+		for i:=0;i!=100000;i++{
+			sum+=i
+		}
+		close(myChl)
+		log.Println("finish")
+		wait.Done()//计数器减一
+		return sum
+	}()
+
+	wait.Wait()//阻塞到任务完成
+
+	log.Println(myChl)
 }
 
