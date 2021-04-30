6202ced2fb8f934b9852894be1645f380029be8e
赵睿 <zrendless@foxmail.com>
2021-04-30 16:06:45 +0800

    使用http.NewServeMux创建多路复用器后,创建server

--- project/Web/HellowWeb/webMain.go
+++ project/Web/HellowWeb/webMain.go
@@ -11,6 +11,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe("0.0.0.0:8080", nil)
+	multiplexer := http.NewServeMux()
+	multiplexer.HandleFunc("/", handler)
+	server := http.Server{
+		Addr:    "127.0.0.1:8080",
+		Handler: multiplexer,
+	}
+	server.ListenAndServe()
 }
