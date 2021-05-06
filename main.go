f215cc344efee0a7cb32ec474a7c9d51dcc640e0
赵睿 <zrendless@foxmail.com>
2021-05-06 10:39:47 +0800

    sync.RWMutex读写锁

--- main.go
+++ main.go
@@ -2,37 +2,36 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"sync"
 )
 
-type MyInterface interface {
-	//大写暴露出来,才能被reflect感知
-	FooMethod0()
+var rwLocker = sync.RWMutex{}
+var wait = sync.WaitGroup{}
+var gNum = 0
+
+func rFoo(num int) {
+	rwLocker.RLock()
+	defer rwLocker.RUnlock()
+	defer wait.Done()
+	fmt.Println("read", num, gNum)
+	gNum++
 }
 
-type Foo struct {
-	string
-}
-
-func (f Foo) FooMethod0() {
-	fmt.Println(f.string)
+func wFoo(num int) {
+	rwLocker.Lock()
+	defer rwLocker.Unlock()
+	defer wait.Done()
+	fmt.Println("write", num, gNum)
+	gNum++
 }
 
 func main() {
-	myFoo := Foo{"test"}
-
-	//类信息获得
-	myType := reflect.TypeOf(myFoo)
-	for i := 0; i != myType.NumField(); i++ {
-		item := myType.Field(i)
-		fmt.Println(item)
+	wait.Add(20)
+	for i := 0; i != 10; i++ {
+		go rFoo(i)
 	}
-
-	//接口方法获得
-	var theInterface MyInterface = &myFoo
-	myInterfaceType := reflect.TypeOf(theInterface)
-	for i := 0; i != myInterfaceType.NumMethod(); i++ {
-		item := myType.Method(i)
-		fmt.Println(item)
+	for i := 0; i != 10; i++ {
+		go wFoo(i)
 	}
+	wait.Wait()
 }
